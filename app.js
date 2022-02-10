efbaa3d75e6fe79c15cc11f1c76f449abcbb10b6
잼민 <79719602+gun2475@users.noreply.github.com>
2022-02-10 23:20:55 +0900

    수정

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.set('port', process.env.PORT||3000);
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/main.html'));
+app.set('port', process.env.PORT || 3000);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'page/main.js'));
 });
 
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
 });
