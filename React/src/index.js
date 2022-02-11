904cd2da1ac80aa1d9980199f92eaac111e2ee0b
잼민 <79719602+gun2475@users.noreply.github.com>
2022-02-12 04:18:20 +0900

    change file

--- React/src/index.js
+++ React/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+import CoinApp from './test/CoinApp';
+import './doit-ui/app.css';
+// import CoinApp from './03/TodoList';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<CoinApp />, document.getElementById('root'));
+// ReactDOM.render(<CoinApp />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
